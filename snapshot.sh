6f30561b999fd5c3547b5de626e926ebaa490653
novi <andrei.novac.96@gmail.com>
2024-06-29 02:52:55 +0300

    Update snapshot.sh

--- snapshot.sh
+++ snapshot.sh
@@ -2,10 +2,19 @@
 
 export $(cat env | xargs)
 
+echo "Docker stop container"
 docker stop ${SERVICE}_${NETWORK}_${SERVER_TYPE}
 
-aria2c -x16 -s16 https://snapshots.kjnodes.com/$SERVICE/snapshot_latest.tar.lz4 -o - | tar -I lz4 -xf - -C "$HOME/.$SERVICE"
+echo "Delete data folder"
+sudo rm -rf "$HOME/.$SERVICE/data"
 
+echo "Download snapshot"
+aria2c -x16 -s16 https://snapshots.kjnodes.com/$SERVICE/snapshot_latest.tar.lz4
+lz4 -c -d snapshot_latest.tar.lz4 | tar -x -C $HOME/.$SERVICE
+rm -rf snapshot_latest.tar.lz4
+
+echo "Docker start container"
 docker start ${SERVICE}_${NETWORK}_${SERVER_TYPE}
 
+echo "Docker logs"
 docker logs ${SERVICE}_${NETWORK}_${SERVER_TYPE}
