05fe022103ca68b27e7fd5c9441c0f4a4deadcab
novi <andrei.novac.96@gmail.com>
2024-06-29 00:25:51 +0300

    add missing params

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,15 @@ if [ ! -d "$HOME/.$SERVICE" ]; then
     echo "Set chain-id $CHAIN_ID"
     $BINARY config chain-id $CHAIN_ID
 
+    echo "Set genesis: $GENESIS"
+    curl -Ls $GENESIS > $HOME/.$SERVICE/config/genesis.json
+
+    echo "Set addrbook: $ADDRBOOK"
+    curl -Ls $ADDRBOOK > $HOME/.$SERVICE/config/addrbook.json
+
+    echo "Set minimum gas price"
+    sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"$MIN_GAS_PRICE\"|" $HOME/.$SERVICE/config/app.toml
+
     # TODO: setup app.toml and config.toml
 fi
 
