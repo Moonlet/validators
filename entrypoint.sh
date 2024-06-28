0c2f887701291aa35c1490511bbc96aa306cdbf2
novi <andrei.novac.96@gmail.com>
2024-06-29 00:30:30 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,16 +8,20 @@ if [ ! -d "$HOME/.$SERVICE" ]; then
     echo "Set chain-id $CHAIN_ID"
     $BINARY config chain-id $CHAIN_ID
 
-    echo "Set genesis: $GENESIS"
-    curl -Ls $GENESIS > $HOME/.$SERVICE/config/genesis.json
-
-    echo "Set addrbook: $ADDRBOOK"
-    curl -Ls $ADDRBOOK > $HOME/.$SERVICE/config/addrbook.json
-
     echo "Set minimum gas price"
     sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"$MIN_GAS_PRICE\"|" $HOME/.$SERVICE/config/app.toml
 
     # TODO: setup app.toml and config.toml
+fi
+
+if [ ! -f "$HOME/.$SERVICE/config/genesis.json" ]; then
+    echo "Set genesis: $GENESIS"
+    curl -Ls $GENESIS > $HOME/.$SERVICE/config/genesis.json
+fi
+
+if [ ! -f "$HOME/.$SERVICE/config/addrbook.json" ]; then
+    echo "Set addrbook: $ADDRBOOK"
+    curl -Ls $ADDRBOOK > $HOME/.$SERVICE/config/addrbook.json
 fi
 
 # Check if the command arguments ($@) are provided
