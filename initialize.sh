126d5626cc1574e1fa5ed467ee50142cee336d50
novi <andrei.novac.96@gmail.com>
2024-06-29 02:40:39 +0300

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -4,12 +4,18 @@ echo "Initialize..."
 
 export $(cat env | xargs)
 
+HOME_DIR=$HOME/.$SERVICE/config
+
+if [ ! -d "$HOME_DIR" ]; then
+    mkdir -p $HOME_DIR
+fi
+
 echo "Set genesis: $GENESIS"
 wget -O genesis.json $GENESIS --inet4-only
-sudo mv genesis.json $HOME/.$SERVICE/config/genesis.json
+sudo mv genesis.json $HOME_DIR/genesis.json
 
 echo "Set addrbook: $ADDRBOOK"
 wget -O addrbook.json $ADDRBOOK --inet4-only
-sudo mv addrbook.json $HOME/.$SERVICE/config/addrbook.json
+sudo mv addrbook.json $HOME_DIR/addrbook.json
 
 echo "Finish initialize."
