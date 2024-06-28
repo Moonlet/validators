7295bb3d46995bb4ce44dab1c9137f5c6036a409
novi <andrei.novac.96@gmail.com>
2024-06-29 02:11:57 +0300

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -2,8 +2,9 @@ echo "Cleanup..."
 
 sudo docker system prune -a
 sudo rm -rf $HOME/.dymension
-cd $HOME/validators && git stash
-cd $HOME/validators && git stash drop
-cd $HOME/validators && git stash pull
+cd $HOME/validators
+git stash
+git stash drop
+git pull
 chmod +x build.sh run.sh
 ./build.sh && ./run.sh
