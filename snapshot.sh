076d82be94fd0c5d2f866f20b2a5278b0808d5a9
novi <andrei.novac.96@gmail.com>
2024-06-29 02:46:03 +0300

    Update snapshot.sh

--- snapshot.sh
+++ snapshot.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-curl -L https://snapshots.kjnodes.com/$SERVICE/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.$SERVICE
+export $(cat env | xargs)
+
+docker stop ${SERVICE}_${NETWORK}_${SERVER_TYPE}
+
+aria2c -x16 -s16 https://snapshots.kjnodes.com/$SERVICE/snapshot_latest.tar.lz4 -o - | tar -I lz4 -xf - -C "$HOME/.$SERVICE"
+
+docker start ${SERVICE}_${NETWORK}_${SERVER_TYPE}
+
+docker logs ${SERVICE}_${NETWORK}_${SERVER_TYPE}
