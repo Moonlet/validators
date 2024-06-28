613f7a2916bf06e2e7f87b22450c6e48b62e4334
novi <andrei.novac.96@gmail.com>
2024-06-29 02:05:44 +0300

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,30 +2,16 @@
 
 echo "Start entrypoint"
 
-# Check if initial setup is required
-# if [ ! -d "$HOME/.$SERVICE" ]; then
-#     echo "Initialize $MONIKER node"
-#     $BINARY init $MONIKER --chain-id $CHAIN_ID
-
-#     echo "Set chain-id $CHAIN_ID"
-#     $BINARY config chain-id $CHAIN_ID
-
-#     echo "Set minimum gas price"
-#     sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"$MIN_GAS_PRICE\"|" $HOME/.$SERVICE/config/app.toml
-
-#     # TODO: setup app.toml and config.toml
-# else
-#     echo "Initial setup has been already made"
-# fi
-
 if [ ! -f "$HOME/.$SERVICE/config/genesis.json" ]; then
     echo "Set genesis: $GENESIS"
-    curl -Ls $GENESIS > $HOME/.$SERVICE/config/genesis.json
+    wget -O genesis.json $GENESIS --inet4-only
+    mv genesis.json $HOME/.$SERVICE/config/genesis.json
 fi
 
 if [ ! -f "$HOME/.$SERVICE/config/addrbook.json" ]; then
     echo "Set addrbook: $ADDRBOOK"
-    curl -Ls $ADDRBOOK > $HOME/.$SERVICE/config/addrbook.json
+    wget -O addrbook.json $ADDRBOOK --inet4-only
+    mv addrbook.json $HOME/.$SERVICE/config/addrbook.json
 fi
 
 # Check if the command arguments ($@) are provided
