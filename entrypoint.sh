ebe8343d1940b85ade3b51a55b9148cb606b0d6e
novi <andrei.novac.96@gmail.com>
2024-07-02 16:49:11 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,13 @@ if [ -z "$1" ]; then
         # --p2p.laddr "tcp://0.0.0.0:$P2P_PORT"
 fi
 
-# envsubst < /root/.${SERVICE}/config/app.toml > /root/.${SERVICE}/config/app.tmp
-# mv /root/.${SERVICE}/config/app.tmp /root/.${SERVICE}/config/app.toml
+envsubst < /root/.${SERVICE}/config/app.toml > /root/.${SERVICE}/config/app.tmp
+mv /root/.${SERVICE}/config/app.tmp /root/.${SERVICE}/config/app.toml
 
-# envsubst < /root/.${SERVICE}/config/client.toml > /root/.${SERVICE}/config/client.tmp
-# mv /root/.${SERVICE}/config/client.tmp /root/.${SERVICE}/config/client.toml
+envsubst < /root/.${SERVICE}/config/client.toml > /root/.${SERVICE}/config/client.tmp
+mv /root/.${SERVICE}/config/client.tmp /root/.${SERVICE}/config/client.toml
 
-# envsubst < /root/.${SERVICE}/config/config.toml > /root/.${SERVICE}/config/config.tmp
-# mv /root/.${SERVICE}/config/config.tmp /root/.${SERVICE}/config/config.toml
+envsubst < /root/.${SERVICE}/config/config.toml > /root/.${SERVICE}/config/config.tmp
+mv /root/.${SERVICE}/config/config.tmp /root/.${SERVICE}/config/config.toml
 
 /$BINARY "$@"
