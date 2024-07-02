7478e0afea2384bfb90e59d20b52a99c9e4b1152
novi <andrei.novac.96@gmail.com>
2024-07-02 08:37:30 +0300

    Create cosmovisor.sh

---
+++ scripts/cosmovisor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VERSION=$1 # v0.37.0
+NETWORK=mainnet
+SERVICE=elys
+BINARY=elysd
+GIT_URL=https://github.com/elys-network/elys/releases/download/$VERSION/elysd-$VERSION-linux-amd64
+
+echo "Start upgrade to version $VERSION"
+
+mkdir -p $HOME/.$SERVICE/cosmovisor/upgrades/$VERSION/bin
+wget -O $HOME/.$SERVICE/cosmovisor/upgrades/$VERSION/bin/$BINARY $GIT_URL
+chmod +x $HOME/.$SERVICE/cosmovisor/upgrades/$VERSION/bin/$BINARY
+
+echo "$BINARY $VERSION moved to cosmovisor upgrades"
