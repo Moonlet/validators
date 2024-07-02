2255c44d8037e460dc27aaaf6e898333a9b975b2
novi <andrei.novac.96@gmail.com>
2024-07-02 14:53:34 +0300

    add envsubst

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,14 @@ if [ -z "$1" ]; then
         --p2p.laddr "tcp://0.0.0.0:$P2P_PORT"
 fi
 
+# Ensure P2P_PORT is set
+if [ -z "$P2P_PORT" ]; then
+  echo "P2P_PORT environment variable is not set. Exiting."
+  exit 1
+fi
+
+# Replace the P2P port in the config.toml file
+envsubst < /home/.${SERVICE}/config/config.toml > /home/.${SERVICE}/config/config.tmp
+mv /home/.${SERVICE}/config/config.tmp /home/.${SERVICE}/config/config.toml
+
 /$BINARY "$@"
